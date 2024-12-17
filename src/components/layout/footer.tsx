06ca678396de87f86e0f021af976e5b868493d85
ivgnod <dongvt@ubox.vn>
2024-12-17 14:18:40 +0700

    update done layout home

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -11,11 +11,40 @@ export const Footer = () => {
             Turns out, all this time I’ve only been doing UI design, haha.
             <br /> Help me achieve my goal of becoming a product designer!
           </h5>
-          <div className="mt-auto flex items-center justify-between">
-            <Link href={"mailto:leminhquyen10122000@gmail.com"}>leminhquyen10122000@gmail.com</Link>
-            <Link href={"tel:+84981268736"}>(+84) 981 268 736</Link>
+          <div className="mt-auto flex items-center justify-between text-2xl leading-9 text-masala-800">
+            <Link href={"mailto:leminhquyen10122000@gmail.com"} className="hover:underline">
+              leminhquyen10122000@gmail.com
+            </Link>
+            <Link href={"tel:+84981268736"} className="hover:underline">
+              (+84) 981 268 736
+            </Link>
           </div>
         </div>
+      </div>
+      <div className="flex items-center justify-between px-10 py-6">
+        <p className="text-base leading-6 text-masala-800">© Minh Quyen Le 2025</p>{" "}
+        <ul className="flex items-center space-x-6">
+          <li>
+            <Link href={"/"} className="transition-all duration-150 ease-linear hover:opacity-80">
+              <img src="home/linkedin.svg" alt="linkedin" />
+            </Link>
+          </li>
+          <li>
+            <Link href={"/"} className="transition-all duration-150 ease-linear hover:opacity-80">
+              <img src="home/facebook.svg" alt="facebook" />
+            </Link>
+          </li>
+          <li>
+            <Link href={"/"} className="transition-all duration-150 ease-linear hover:opacity-80">
+              <img src="home/behance.svg" alt="behance" />
+            </Link>
+          </li>
+          <li>
+            <Link href={"/"} className="transition-all duration-150 ease-linear hover:opacity-80">
+              <img src="home/dribble.svg" alt="dribble" />
+            </Link>
+          </li>
+        </ul>
       </div>
     </footer>
   )
